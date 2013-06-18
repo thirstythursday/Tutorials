a9e8f82e5f37035e9a0da7f10dc2f00edf8cd67b
george <george@10.0.1.10>
2013-06-17 20:17:42 -0400

    Continuing paint demo Signed-off-by:george <george@10.0.1.10>

--- src/georgeoross/GUI.java
+++ src/georgeoross/GUI.java
@@ -12,16 +12,24 @@ import javax.swing.JLabel;
  * @author george
  */
 public class GUI implements Runnable {
-    
+
     @Override
     public void run() {
+
+        paintScene();
+    }
+
+    public void paintScene() {
         JFrame f = new JFrame("George's JFrame Hello World");
         JLabel l = new JLabel("George's JLabel Hello World", JLabel.CENTER);
         f.add(l);
-        f.setSize(300,300);
+        f.setSize(300, 300);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);   
+        f.setVisible(true);
+
+        GraphicsCar car = new GraphicsCar(100, 100);
+        f.add(car);
+        GraphicsCar carTurned = new GraphicsCar(150, 150);
+        carTurned.changeFacing(90);
     }
-            
-    
 }
